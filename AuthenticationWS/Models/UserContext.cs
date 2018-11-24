23c383d18fb466c961dab2eb8cf681e7a638ba87
Edgaras Kazlauskas <edgarasvilija@gmail.com>
2018-11-24 16:59:29 +0100

    Added AuthenticateUser query

---
+++ AuthenticationWS/Models/UserContext.cs
@@ -0,0 +1,19 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace AuthenticationWS.Models
+{
+    public class UserContext
+    {
+        public string ConnectionString { get; set; }
+
+        public UserContext(string connectionString)
+        {
+            this.ConnectionString = connectionString;
+        }
+
+        private MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(ConnectionString);
+        }
+    }
+}
---
+++ AuthenticationWS/Utilities/Queries.cs
@@ -0,0 +1,7 @@
+﻿namespace AuthenticationWS.Utilities
+{
+    public static class Queries
+    {
+        public const string AuthenticateUser = "SELECT * from user WHERE UserName = @UserName AND UserPassword = @UserPassword;";
+    }
+}
