20279584b2f0db1d95917097b39e5f88f2fddceb
Edgaras Kazlauskas <edgarasvilija@gmail.com>
2018-11-24 16:16:41 +0100

    Added User model class

---
+++ AuthenticationWS/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace AuthenticationWS.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string UserPassword { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Country { get; set; }
+        public int IsAdmin { get; set; }
+    }
+}
