21c8ff7bf08df8957257794ea6c37ea6c7d4cac2
Edgaras Kazlauskas <edgarasvilija@gmail.com>
2018-12-18 19:15:55 +0100

    Added SHA2 password hashing

--- AuthenticationWS/Utilities/Queries.cs
+++ AuthenticationWS/Utilities/Queries.cs
@@ -2,11 +2,11 @@
 {
     public static class Queries
     {
-        public const string AuthenticateUser = "SELECT count(*) from user WHERE UserName = @UserName AND UserPassword = @UserPassword;";
+        public const string AuthenticateUser = "SELECT count(*) from user WHERE UserName = @UserName AND UserPassword = SHA2(@UserPassword, 224);";
 
-        public const string GetUserIdEmailIsAdmin = "SELECT UserId, Email, IsAdmin from user WHERE UserName = @UserName AND UserPassword = @UserPassword;";
+        public const string GetUserIdEmailIsAdmin = "SELECT UserId, Email, IsAdmin from user WHERE UserName = @UserName AND UserPassword = SHA2(@UserPassword, 224);";
 
         public const string CreateUser = "INSERT INTO user (UserName, UserPassword, FirstName, LastName, Email, PhoneNumber, Country, IsAdmin)\n" +
-                    "VALUES (@userName, @userPassword, @firstName, @lastName, @email, @phoneNumber, @country, @isAdmin);";
+                    "VALUES (@userName, SHA2(@userPassword, 224), @firstName, @lastName, @email, @phoneNumber, @country, @isAdmin);";
     }
 }
