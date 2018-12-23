b2061759ff498b5abef42dea458ae96e562231b7
Edgaras Kazlauskas <edgarasvilija@gmail.com>
2018-12-23 17:14:50 +0100

    More readable querries

--- AuthenticationWS/Utilities/Queries.cs
+++ AuthenticationWS/Utilities/Queries.cs
@@ -2,9 +2,9 @@
 {
     public static class Queries
     {
-        public const string AuthenticateUser = "SELECT count(*) from user WHERE UserName = @UserName AND UserPassword = SHA2(@UserPassword, 224);";
+        public const string AuthenticateUser = "SELECT COUNT(*) FROM user WHERE UserName = @UserName AND UserPassword = SHA2(@UserPassword, 224);";
 
-        public const string GetUserIdEmailIsAdmin = "SELECT UserId, Email, IsAdmin from user WHERE UserName = @UserName AND UserPassword = SHA2(@UserPassword, 224);";
+        public const string GetUserIdEmailIsAdmin = "SELECT UserId, Email, IsAdmin FROM user WHERE UserName = @UserName AND UserPassword = SHA2(@UserPassword, 224);";
 
         public const string CreateUser = "INSERT INTO user (UserName, UserPassword, FirstName, LastName, Email, PhoneNumber, Country, IsAdmin)\n" +
                     "VALUES (@userName, SHA2(@userPassword, 224), @firstName, @lastName, @email, @phoneNumber, @country, @isAdmin);";
